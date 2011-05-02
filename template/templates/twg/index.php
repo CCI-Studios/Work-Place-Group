0eaffa865a65b73b375908e6286e7ce7143e0fe2
Darren Murray <dmurray@ccistudios.com>
2011-05-02 12:01:51 -0400

    updating files

--- template/templates/twg/index.php
+++ template/templates/twg/index.php
@@ -1,17 +1,21 @@
-<!DOCTYPE>
-
+<!DOCTYPE html>
 
 <html>
-
 <head>
-	
-	<link rel="shortcut icon" href="/templates/twg/images/favicon.ico" 
+	<link rel="shortcut icon" href="/templates/twg/images/favicon.ico" />
+	<link rel="stylesheet" href="/templates/twg/css/template.css" />
 </head>
 
 <body>
+	<div id="top">
+		<div>top section</div>
+	</div>
+	
+	<div id="mainmenu">
+		<div>main menu</div>
+	</div>
 	
 	
 
 
 </body>
-</html>
