aa063556f3c6e6df9cf4e9bf88336fd7976293c4
Darren Murray <dmurray@ccistudios.com>
2011-05-03 17:03:56 -0400

    more additions

--- template/templates/twg/index.php
+++ template/templates/twg/index.php
@@ -9,29 +9,36 @@
 
 <body>
 	<div id="top">
-		<jdoc:include type="modules" name="logo" style="xhtml" />
-		<jdoc:include type="modules" name="topmenu" style="xhtml" />
+			<jdoc:include type="modules" name="top" style="xhtml" />
 	</div>
 	
-	<div id="mainmenu">
-			<jdoc:include type="modules" name="mainmenu" style="xhtml" />
-	</div>
-	
-	<div id="content">
-	<div id="sidebar">
-		<div></div>
-		<jdoc:include type="modules" name="sidebar" style="xhtml" />
+	<div id="mainmenu"><div>
+		<jdoc:include type="modules" name="mainmenu" style="xhtml" />
 	</div></div>
 	
-	<div class="clear"></div>
-	
+	<div id="content">
+		<div id="main">
+			<div id="component">
+				<jdoc:include type="component" />
+			</div>
+			<div id="bottom">
+				<jdoc:include type="modules" name="bottom" style="xhtml" />
+			</div>
+			<div id="bottomdummy">
+			</div>
+		</div>
+		<div id="sidebar">
+			<jdoc:include type="modules" name="sidebar" style="xhtml" />
+		</div>
+		<div class="clear"></div>
+	</div></div></div>
+		
 	<div id="footer">
 		<div>Site by CCI Studios
-			<img src="../templates/twg/images/employmentontariologo.png" width="78" height="24" alt="Employmentontariologo">
+			<img src="../templates/twg/images/employmentontariologo.png" width="78" height="24" alt="Employmentontariologo" />
 			Copyright 2008 The WorkPlace Group
-			<img src="../templates/twg/images/twglogomini.png" width="25" height="25" alt="Twglogomini">
-			<jdoc:include type="modules" name="footer" style="xhtml">
-		</div>	
+			<img src="../templates/twg/images/twglogomini.png" width="25" height="25" alt="Twglogomini" />
+		</div>
 	</div>
 
 </body>
